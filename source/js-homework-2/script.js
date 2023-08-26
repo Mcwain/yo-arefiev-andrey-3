6ef040ed151d8d4dd5b27352851a254ae7c70092
Mcwain <mcwaintlt@gmail.com>
2023-08-26 23:58:34 +0400

    Update script.js

--- source/js-homework-2/script.js
+++ source/js-homework-2/script.js
@@ -1,14 +1,7 @@
-// const num= prompt ("enter the number");
-// const degree= prompt ("enter the degree");
-// for (i=1; i<=num; i++);
-// for (a=1; a<=degree; a++) {
-//     result=(i-1)**a;
-// }
-// console.log(result);
-let num=prompt("Введите число");
-let degree=prompt("Введите степень");
-let result=1;
-// for (i=1; i<=degree; i++){
-result*=num;
-
-console.log( "Число"+ " " + num + " в степени " + degree + " равняется " + result);
+const num= prompt ("enter the number");
+const degree= prompt ("enter the degree");
+for (i=1; i<=num; i++);
+for (a=1; a<=degree; a++) {
+    result=(i-1)**a;
+}
+console.log(result);
